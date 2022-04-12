a49ea1c90b49ca561148747c7be133f7aa66d965
Meitar Segal <meitarsegal@YC-HQ-Meitar.local>
2022-04-12 10:14:37 +0200

    uploading folder structure for the 2 tutorials

---
+++ video-tuts/brian-will-tuts/factorial.clj
@@ -0,0 +1,34 @@
+; Will throw overflow exception for n greater
+; than 20 because * only accepts and returns Long values.
+; Recursive
+
+(defn factorial [n]
+  (if (= n 0) 1 
+      (* n (factorial (dec n)))))
+
+(factorial 20)
+
+
+; Uses arbitrary precision math function, so it works for n greater than 20.
+; On different systems it will reach different stacks before a stack overflow happens
+; (this will vary from systme to system.)
+
+(defn factorial-bigint [n]
+  (if (= n 0) 1
+      ;the function *' returns arbitrary precision numbers (clojure.lang.BigIng)
+      (*' n (factorial-bigint (dec n)))))
+
+(factorial-bigint 21)
+
+
+(defn factorial-bigint-recur [n]
+  (if (n = 0) 1
+      ; EVALUTAION ERROR! recur is not in tail position
+      (* n (recur (dec n)))))
+
+; does not consume additional stack space for each iteration because it is uing 'recur' fn
+(defn factorial-bigint-loop [n]
+  (if (= n 0) 1 
+     (loop [val n i n]
+       (if (<= i 1) val 
+           (recur (*' val (dec i)) (dec i))))))
---
+++ video-tuts/kelvin-mai-tuts/user.clj
@@ -0,0 +1,61 @@
+;; (+ 1 2 3 5)
+
+;; ; "the ; is the comments in clojures"
+
+;; (type "String")
+;; (type 11)
+;; (type nil)
+;; (type 12.0)
+
+;; ; 'something = tells clojure to ignore evalutaings the next thing
+;; ; ^ the above is called a symbol
+
+;; 'something
+
+
+;; vars
+;; (def temp
+;;   'something)
+
+;; temp
+
+;; (comment
+;;   ;; comment form so that the code does not actually get submitted
+;;   )
+
+
+;; (vector 1 2 3)
+
+;; (def temp-vector
+;;   [1 2 3 "something" \c])
+
+;; (def temp-list 
+;;   (list 1 2 3))
+
+;; `(1 2 3)
+;; ;; list -> list programming
+;; ;; everything is DATA
+
+;; (conj temp-vector 10)
+;; (conj temp-list 10)
+
+;; ;; Note for how conj list its added to front but conj vector added to the end
+
+;; ;;polymorphism
+;; (coll? temp-vector)
+;; (seq? temp-vector)
+;; (seq temp-vector)
+
+;; (second temp-list)
+
+(set [1 2 3 3])
+
+(def temp-set
+  #{1 2 3})
+
+temp-set
+(comment
+  temp-vector
+  temp-list
+  ;;
+  )
