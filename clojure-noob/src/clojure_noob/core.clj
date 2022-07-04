26cac43ea877ae158315162ddc392e65803302ea
Meitie <meitarsegal@gmail.com>
2022-07-04 16:21:54 +0200

    study

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -381,3 +381,37 @@
 ;; => ("Hi, Darth Vader" "Hi, Mr. Magoo")
 
 ;; If your annon fn houses more then 1 arg, you can call them with %n, e.g %1 %2 %3
+
+;; ;; Funciton call
+;; (* 8 3)
+
+;; ;; Anon Fn
+;; #(* % 3)
+;; #(* %1 %2 %3 4)
+
+(#(str %1 " and " %2) "cornbread" "butter beans")
+; => "cornbread and butter beans"
+
+(#(identity %&) 1 "blarg" :yip)
+; => (1 "blarg" :yip)
+
+
+(def asym-hobbit-body-parts 
+  [{:name "head" :size 3}
+   {:name "left-ear" :size 1}
+   {:name "mouth" :size 1}
+   {:name "nose" :size 1}
+   {:name "neck" :size 2}
+   {:name "left-shoulder" :size 3}
+   {:name "left-upper-arm" :size 3}
+   {:name "chest" :size 10}
+   {:name "back" :size 10}
+   {:name "left-forearm" :size 3}
+   {:name "abdomen" :size 6}
+   {:name "left-kidney" :size 1}
+   {:name "left-hand" :size 2}
+   {:name "left-knee" :size 2}
+   {:name "left-thigh" :size 4}
+   {:name "left-lower-leg" :size 3}
+   {:name "left-achilles" :size 1}
+   {:name "left-foot" :size 2}])
