4ca8478c6526f469bfee35cc42281bc1c313279e
Meitar <meitarsegal@gmail.com>
2022-08-17 11:39:17 +0200

    args / arity / destructuring / rests

--- brave-clj/basics.clj
+++ brave-clj/basics.clj
@@ -139,3 +139,137 @@ failed-protagonist-names
 (nth '(1 2 3 4) 3)
 
 (conj '(1 2 3) 4)
+
+;; SETS
+#{"kurt" 20 :icicle}
+(hash-set 1 1 2 2)
+
+(conj #{:a :b} :a)
+
+(set [3 3 3 3 3 3 3 4 4])
+
+(contains? #{:a :b} :a)
+(contains? #{:a :b} 3)
+(contains? #{nil} nil)
+
+(:a #{:a :b})
+
+(get #{:a :b} :a)
+(get #{:a nil} nil)
+(get #{:a :b} "kurt")
+
+
+;; FUNCTIONS
+
+;; calling functions
+(+ 1 2 3 4)
+(* 1 2 3 4)
+(first [1 2 3 4])
+
+(or + -)
+((or + - ) 1 2 3)
+
+((and (= 1 1) +) 1 2 3)
+((first [+ 0]) 1 2 3)
+
+(inc 1.1)
+(map inc [0 1 2 3])
+
+(+ (inc 199) (/ 100 (- 7 2)))
+(+ 200 (/ 100 (- 7 2)))
+(+ 200 (/ 100 5))
+(+ 200 20)
+220
+
+(defn too-enthusiastic
+  "Return a cheer"
+  [name]
+  (str "OH MY GOSH " name " Are the best!"))
+
+(too-enthusiastic "Zelda")
+
+(defn no-params
+  []
+  "I take no parameters!")
+
+(defn one-param
+  [x]
+  (str "I take one parameter: " x))
+
+(defn two-params
+  [x y]
+  (str "Two params! I take " x " and " y))
+
+(def do-things )
+
+(defn multi-arity
+  ;; 3-arity arugments and body
+  ([first-arg second-arg third-arg]
+   (do-things first-arg second-arg third-arg))
+  ([first-arg second-arg] 
+   (do-things first-arg second-arg)) 
+  ([first-arg]
+   (do-things first-arg)))
+
+(defn x-chop
+  "Describe the kind of chop you're inflecting on someone"
+  ([name chop-type]
+   (str "I " chop-type " chop " name "!"))
+  ([name]
+   x-chop name "Karate"))
+
+;; Another way of writing the above code is as follows.
+(defn x-chop2 [name & [chop-type]]
+  (str "I " (or chop-type "Karate") " chop " name "!"))
+
+(x-chop2 "harry" "slap")
+
+(defn weird-arity
+  ([]
+   "No Arity will return this string")
+  ([number]
+   (inc number)))
+
+(defn codger-communication
+  [whippersnapper]
+  (str "Get off my lawn, " whippersnapper "!!!"))
+
+(defn codger 
+  [& whippersnappers]
+  (map codger-communication whippersnappers))
+
+(codger "billy" "annie" "hulk")
+
+(defn favorite-things
+  [name & things]
+  (str "Hi, " name ", here are my fav things: "
+       (clojure.string/join ", " things)))
+
+(favorite-things "Doreen" "cars")
+
+;; Destructuring
+
+(defn my-first
+  [[first-thing second-thing]]
+  second-thing)
+(my-first ["oven" "car" "bike"])
+
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "Your first choice is: " first-choice))
+  (println (str "Your second choice is: " second-choice))
+  (println (str "Your unimportant choice is: " 
+                (clojure.string/join ", " unimportant-choices))))
+(chooser ["Marmalade" "Handsome Jack" "Pigpen" "Aquaman"])
+
+(defn announce-treasure-location
+  [{lat :lat lng :lng}]
+  (println (str "Treausre Lat: " lat))
+  (println (str "Treausre lng: " lng)))
+
+(defn announce-treasure-location2 
+  [{:keys [lat lng]}]
+  (println (str "Treausre lat: " lat))
+  (println (str "Treausre lng: " lng)))
+
+(announce-treasure-location {:lat 28.22 :lng 81.33})
