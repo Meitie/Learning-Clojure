a44c39bb66a4d2c97dfbcbb1684e7e4140eef01b
Meitar <meitarsegal@gmail.com>
2022-08-25 16:05:14 +0200

    chapter 4 up until fitler and some

--- brave-clj/basics.clj
+++ brave-clj/basics.clj
@@ -541,3 +541,74 @@ failed-protagonist-names
     #(- % x))
 (def dec9 (dec-maker 9))
 (dec9 10)
+
+
+;; CLOJURE CORES
+;; using seq
+(defn titleize
+  [topic]
+  (str topic " for the Brave and True"))
+
+(map titleize ["Hamsters" "Ragnarok"])
+(map titleize '("Empathy" "Decorating"))
+(map titleize #{"Elbows" "Soap Carving"})
+(map #(titleize (second %)) {:uncomfortable-thing "Winking"})
+
+(seq '(1 2 3))
+(seq [1 2 3])
+(seq #{1 2 3})
+(seq {:name "bill" :occupation "lol"})
+
+(into {} (seq {:a 1 :b 2 :c 3}))
+
+;; map
+(map inc [1 2 3])
+(map str ["a" "b" "c"] ["A" "B" "C"])
+;;as if map does the following
+(list (str "a" "A") (str "b" "B") (str "c" "C"))
+
+
+;;examples
+(def human-consumption [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
+
+(map unify-diet-data human-consumption critter-consumption)
+
+
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+
+(stats [3 4 10])
+
+(stats [80 1 44 13 6])
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
+
+(map :alias identities)
+
+;; reduce
+(reduce (fn [new-map [key val]]
+          (assoc new-map key (inc val)))
+        {}
+        {:max 30 :min 10})
+
+(reduce (fn [new-map [key val]]
+          (if (> val 4)
+            (assoc new-map key val)
+            new-map))
+        {}
+        {:human 4.1
+         :critter 3.9})
+
+;; take / drop / take-while / drop-while
